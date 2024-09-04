578139def955d38f2eef6f6c2ca5a2a4624ed3e9
steviels <118238179+steviels@users.noreply.github.com>
2024-09-04 13:07:29 -0400

    added home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import HomePage from "./components/pages/Home/HomePage.jsx";
 
 export default function App () {
+
   return (
     <BrowserRouter basename = "/">
       <Routes>
-        <Route path = "*" element = {<div></div>} />
+        <Route path = "/" element = {<HomePage />} />
+        <Route path = "*" element = {<Navigate to = "/" />} />
       </Routes>
     </BrowserRouter>
   )
